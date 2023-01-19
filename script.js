08a32656d025767530fbc4da4665d7837b1f40d0
Maikon Franco <122401056+Maikon-V2F@users.noreply.github.com>
2023-01-19 08:42:08 -0300

    Version 1.0
    Initial commit, the first version of the game.

---
+++ script.js
@@ -0,0 +1,37 @@
+const mario = document.querySelector('.mario')
+const plant = document.querySelector('.plant')
+const sound = document.querySelector('audio')
+
+const jump = () => {
+    mario.classList.add('jump')
+
+    setTimeout(() => {
+        mario.classList.remove('jump')}
+        ,1000)
+}
+
+const loop = setInterval(() => {
+    
+    const plantPosition = plant.offsetLeft
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
+    console.log(marioPosition)
+    
+    if (plantPosition <= 300 && marioPosition < 300 && plantPosition > 0 ) {
+        
+        plant.style.animation = 'none'
+        plant.style.left = `${plantPosition}px`
+        
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosition}px`
+
+        mario.src = './assets/death.gif'
+        mario.style.width = '50vh'
+
+        sound.play()
+
+        clearInterval(loop)
+    }
+
+}, 10)
+
+document.addEventListener('keydown', jump)
